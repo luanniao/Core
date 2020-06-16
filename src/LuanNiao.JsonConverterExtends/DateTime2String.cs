ff970aa46b55cd7cea793ae7282a3c24b040b653
BenLampson <58603432@qq.com>
2020-06-16 17:09:13 +0800

    add the json extends project
    add the json extends project

---
+++ src/LuanNiao.JsonConverterExtends/DateTime2String.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace LuanNiao.JsonConverterExtends
+{
+    public class DateTime2String : JsonConverter<DateTime>
+    {
+
+        private readonly string _format;
+        public DateTime2String(string format)
+        {
+            _format = format;
+        }
+
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var raw = reader.GetString();
+            if (DateTime.TryParse(raw, out var data))
+            {
+                return data;
+            }
+            return DateTime.MinValue;
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString(_format));
+        }
+    }
+}
---
+++ src/LuanNiao.JsonConverterExtends/Long2String.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace LuanNiao.JsonConverterExtends
+{
+    public class Long2String : JsonConverter<long>
+    {
+        public override long Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var rawData = reader.GetString();
+            if (Int64.TryParse(rawData, out var data))
+            {
+                return data;
+            }
+            return 0;
+        }
+
+        public override void Write(Utf8JsonWriter writer, long value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+}

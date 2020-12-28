0d4c07902348ac8f9ad3101e0abf89048c21c22e
BenLampson(赵元源) <58603432@qq.com>
2020-12-28 09:33:03 +0800

    publish .NET 5 Version
    publish .NET 5 Version

---
+++ src/Extends/LuanNiao.JsonConverterExtends/DateTime2StringAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace LuanNiao.JsonConverterExtends
+{
+    public class DateTime2StringAttribute : JsonConverterAttribute
+    {
+        private readonly string _format;
+        public DateTime2StringAttribute(string format = "yyyy-MM-dd")
+        {
+            _format = format;
+        }
+
+        public override JsonConverter CreateConverter(Type typeToConvert)
+        {
+
+            if (typeToConvert != typeof(DateTime) && typeToConvert != typeof(DateTime?))
+            {
+                throw new InvalidOperationException($"{nameof(DateTime2StringAttribute)} just can use to type:{typeof(DateTime)} or type:{typeof(DateTime?)}");
+            }
+
+            if (typeToConvert == typeof(DateTime))
+            {
+                return new DateTime2StringConverter(_format);
+            }
+            else if (typeToConvert == typeof(DateTime?))
+            {
+                return new NullableDateTime2StringConverter(_format);
+            }
+            else
+            {
+                throw new InvalidOperationException($"{nameof(DateTime2StringAttribute)} just can use to type:{typeof(DateTime)} or type:{typeof(DateTime?)}");
+            }
+
+        }
+
+    }
+}
---
+++ src/Extends/LuanNiao.JsonConverterExtends/NullableDateTime2StringConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace LuanNiao.JsonConverterExtends
+{
+    public class NullableDateTime2StringConverter : JsonConverter<DateTime?>
+    {
+
+        private readonly string _format;
+        public NullableDateTime2StringConverter(string format)
+        {
+            _format = format;
+        }
+
+        public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            try
+            {
+                var raw = reader.GetString();
+                if (DateTime.TryParse(raw, out var data))
+                {
+                    return data;
+                }
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime? value, JsonSerializerOptions options)
+        {
+            if (value == null)
+            {
+                writer?.WriteStringValue((string)null);
+            }
+            else
+            {
+                writer?.WriteStringValue(value.Value.ToString(_format));
+            }
+        }
+    }
+}

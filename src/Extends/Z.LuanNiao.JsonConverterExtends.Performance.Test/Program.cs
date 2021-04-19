0b221aa5d6fdebd7c748cd597da55964d714ccb9
BenLampson(赵元源) <58603432@qq.com>
2021-04-19 19:40:33 +0800

    Update Program.cs

--- src/Extends/Z.LuanNiao.JsonConverterExtends.Performance.Test/Program.cs
+++ src/Extends/Z.LuanNiao.JsonConverterExtends.Performance.Test/Program.cs
@@ -5,7 +5,7 @@ namespace Z.LuanNiao.JsonConverterExtends.Performance.Test
 {
     public class Program
     {
-        public static void Main(string[] _)
+        public static void Main()
         {
             _ = BenchmarkRunner.Run<JsonTest>();
         }

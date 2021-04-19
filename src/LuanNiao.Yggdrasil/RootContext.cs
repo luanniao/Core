9700c2292c7e0d86b54631ee552e8e751f0dc28f
BenLampson(赵元源) <58603432@qq.com>
2021-04-19 19:39:55 +0800

    simple version

--- src/LuanNiao.Yggdrasil/RootContext.cs
+++ src/LuanNiao.Yggdrasil/RootContext.cs
@@ -1,8 +1,31 @@
 ﻿using System;
+using System.Reflection;
+using System.Runtime.Loader;
 
 namespace LuanNiao.Yggdrasil
 {
-    public class RootContext
+    public class RootContext : AssemblyLoadContext
     {
+        // Resolver of the locations of the assemblies that are dependencies of the
+        // main plugin assembly.
+        private readonly AssemblyDependencyResolver _resolver;
+
+        public RootContext(string pluginPath) : base(isCollectible: true)
+        {
+            _resolver = new AssemblyDependencyResolver(pluginPath);
+        }
+        
+        protected override Assembly Load(AssemblyName name)
+        {
+            string assemblyPath = _resolver.ResolveAssemblyToPath(name);
+            if (assemblyPath != null)
+            {
+                Console.WriteLine($"Loading assembly {assemblyPath} into the HostAssemblyLoadContext");
+                return LoadFromAssemblyPath(assemblyPath);
+            }
+
+            return null;
+        }
     }
+
 }
